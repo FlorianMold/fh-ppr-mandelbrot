ae5341e1dfee94859452e9aa1bcd60813d340157
Florian Mold <florian.mold@agnet.at>
2022-03-11 21:17:28 +0100

    init workspace

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+#pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        printf("hello  %d\n", ID);
+        printf("hello again %d\n", ID);
+    }
+    return 0;
+}
