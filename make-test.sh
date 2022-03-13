714e9d2082d278a2610888573daddf1691e9d078
Florian Mold <florian.mold@agnet.at>
2022-03-13 14:25:13 +0100

    chore: set maxIterations in execution scripts

--- make-test.sh
+++ make-test.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-while getopts i:t:f: flag
+while getopts i:t:f:m: flag
 do
     case "${flag}" in
         i) iterations=${OPTARG};;
         t) threads=${OPTARG};;
         f) filename=${OPTARG};;
+        m) maxIterations=${OPTARG};;
     esac
 done
 
-echo "Running: $iterations iterations";
+echo "Running: $iterations iterations of the program";
+echo "Mandelbrot is run with: $maxIterations max-iterations";
 echo "Running: $threads threads";
-echo "Saving to: $filename threads";
+echo "Saving to: $filename";
 
-for j in  $(seq 1 $iterations);
+for j in $(seq 1 $iterations);
 do
-   ./cmake-build-debug/fh_ppr_mandelbrot $threads >> $filename
+   ./cmake-build-debug/fh_ppr_mandelbrot $threads $maxIterations  >> $filename
 done
--- run-test.sh
+++ run-test.sh
@@ -1,8 +1,22 @@
-./make-test.sh -i 100 -t 1 -f results/result-t1.txt
-./make-test.sh -i 100 -t 2 -f results/result-t2.txt
-./make-test.sh -i 100 -t 3 -f results/result-t3.txt
-./make-test.sh -i 100 -t 4 -f results/result-t4.txt
-./make-test.sh -i 100 -t 5 -f results/result-t5.txt
-./make-test.sh -i 100 -t 6 -f results/result-t6.txt
-./make-test.sh -i 100 -t 7 -f results/result-t7.txt
-./make-test.sh -i 100 -t 8 -f results/result-t8.txt
+# Clear directory
+rm results-maxiterations-100/*
+rm results-maxiterations-500/*
+rm results-maxiterations-1000/*
+
+echo "Running tests with 100 max-iterations"
+for j in $(seq 1 16);
+do
+   ./make-test.sh -i 100 -t ${j} -m 100 -f results-maxiterations-100/result-t${j}.txt
+done
+
+echo "Running tests with 500 max-iterations"
+for j in $(seq 1 16);
+do
+   ./make-test.sh -i 100 -t ${j} -m 500 -f results-maxiterations-500/result-t${j}.txt
+done
+
+echo "Running tests with 1000 max-iterations"
+for j in $(seq 1 16);
+do
+   ./make-test.sh -i 100 -t ${j} -m 1000 -f results-maxiterations-1000/result-t${j}.txt
+done
