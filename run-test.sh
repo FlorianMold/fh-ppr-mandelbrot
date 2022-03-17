09ceebd28f59a282f4cf9f1711f6049f92bafa22
Florian Mold <florian.mold@agnet.at>
2022-03-17 16:30:14 +0100

    refactor: correct path in execution script

--- run-test.sh
+++ run-test.sh
@@ -1,22 +1,22 @@
 # Clear directory
-rm results-maxiterations-100/*
-rm results-maxiterations-500/*
-rm results-maxiterations-1000/*
+rm results/maxiterations-100/*
+rm results/maxiterations-500/*
+rm results/maxiterations-1000/*
 
 echo "Running tests with 100 max-iterations"
 for j in $(seq 1 16);
 do
-   ./make-test.sh -i 100 -t ${j} -m 100 -f results-maxiterations-100/result-t${j}.txt
+   ./make-test.sh -i 100 -t ${j} -m 100 -f results/maxiterations-100/result-t${j}.txt
 done
 
 echo "Running tests with 500 max-iterations"
 for j in $(seq 1 16);
 do
-   ./make-test.sh -i 100 -t ${j} -m 500 -f results-maxiterations-500/result-t${j}.txt
+   ./make-test.sh -i 100 -t ${j} -m 500 -f results/maxiterations-500/result-t${j}.txt
 done
 
 echo "Running tests with 1000 max-iterations"
 for j in $(seq 1 16);
 do
-   ./make-test.sh -i 100 -t ${j} -m 1000 -f results-maxiterations-1000/result-t${j}.txt
+   ./make-test.sh -i 100 -t ${j} -m 1000 -f results/maxiterations-1000/result-t${j}.txt
 done
